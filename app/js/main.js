c499093f200af4c0d86b293372c450ee5dee88f5
santiagopagnone <spagnone@gmail.com>
2019-01-25 22:40:16 -0300

    side nav bar init

---
+++ app/js/main.js
@@ -0,0 +1,112 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const config = require(path.join(__dirname, '../../package.json'))
+
+
+const  { app,shell, BrowserWindow, Menu} = electron;
+
+
+let mainWindow;
+
+
+//Listen for app ready
+app.on('ready', function(){
+    //Create new window
+    mainWindow = new BrowserWindow({
+      backgroundColor: 'lightgray',
+      title: config.getName,
+      show: true,
+      webPreferences: {
+        nodeIntegration: true,
+        defaultEncoding: 'UTF-8'
+      }
+    });
+    //Load html file into window
+    mainWindow.loadURL(url.format({
+        pathname : path.join(__dirname,'../mainWindow.html'), 
+        protocol: 'file', 
+        slashes: true
+    }));
+    createMenu();
+});
+
+function createMenu() {
+    const template = [];
+    // Edit Menu
+    template.push({
+       label: 'Edit',
+       submenu: [
+          { role: 'undo' },
+          { role: 'redo' },
+          { type: 'separator' },
+          { role: 'cut' },
+          { role: 'copy' },
+          { role: 'paste' },
+          { role: 'pasteandmatchstyle' },
+          { role: 'delete' },
+          { role: 'selectall' }
+       ]
+    });
+    // View Menu
+    template.push({
+       label: 'Views',
+       submenu: [
+          { role: 'reload' },
+          { role: 'forcereload' },
+          { role: 'toggledevtools' },
+          { type: 'separator' },
+          { role: 'resetzoom' },
+          { role: 'zoomin' },
+          { role: 'zoomout' },
+          { type: 'separator' },
+          { role: 'togglefullscreen' }
+       ]
+    });
+    // Windown menu
+    template.push({
+       role: 'window',
+       submenu: [{ role: 'minimize' }, { role: 'close' }]
+    });
+    // Help menu
+    template.push({
+       role: 'help',
+       submenu: [
+          {
+             label: 'Learn More',
+             click() {
+                require('electron').shell.openExternal('https://electron.atom.io');
+             }
+          }
+       ]
+    });
+
+    if (process.platform === 'darwin') {
+       template.unshift({
+          label: app.getName(),
+          submenu: [
+             { role: 'about' },
+             { type: 'separator' },
+             { role: 'services', submenu: [] },
+             { type: 'separator' },
+             { role: 'hide' },
+             { role: 'hideothers' },
+             { role: 'unhide' },
+             { type: 'separator' },
+             { role: 'quit' }
+          ]
+       });
+
+       // Edit menu
+       template[1].submenu.push(
+          { type: 'separator' },
+          { label: 'Speech', submenu: [{ role: 'startspeaking' }, { role: 'stopspeaking' }] }
+       );
+
+       // Window menu
+       template[3].submenu = [{ role: 'close' }, { role: 'minimize' }, { role: 'zoom' }, { type: 'separator' }, { role: 'front' }];
+    }
+
+    menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+ }
